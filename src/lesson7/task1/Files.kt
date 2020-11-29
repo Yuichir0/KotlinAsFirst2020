cbd1c25e7efedf5538a943a1fdb68838f5808e9c
Daniil <yuichir0@mail.ru>
2020-11-29 18:51:44 +0300

    lesson 7

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -326,7 +326,7 @@ fun markdownToHtmlSimple(inputName: String, outputName: String) {
     var switch: Boolean
     for (line in File(inputName).readLines()) {
         var j = -1
-        val parts = line.chunked(1)
+        val parts = line.replace(Regex("[\\s\\t]"), "").chunked(1)
         if (line.isEmpty() && check5) writer.write("</p>")
         else
             for (part in parts) {
